134fbbeed4f8eed84b3a7db28d167db451ab1381
Sean Orosz <seanorosz@yahoo.com>
2022-02-15 15:25:33 -0500

    V1.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import getPhotos from './service.js';
 
-getPhotos();
+class App extends React.Component {
+    render() {
+        return (
+            <Images />
+        );
+    }
+}
+
+class Images extends React.Component {
+    constructor(props) {
+        super(props);
+        const photoObj = {};
+        this.state = {
+            photoObj: photoObj,
+        }
+    }
+
+    componentDidMount() {
+        this.callApi();
+    }
+    callApi() {
+        let photos;
+        getPhotos('Dog').then(data => {
+            photos = data.photos;
+            console.log(photos);
+        });
+        this.setState({
+            photoObj: photos,
+        });
+    }
+    
+
+    render() {
+        const pics = this.state.photoObj;
+        console.log(pics)
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/service.js
+++ src/service.js
@@ -1,12 +1,10 @@
-const getPhotos = async () => {
-    const query = 'Pug';
+const getPhotos = async (query) => {
     const response = await fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=10`, {
         headers: {
-            Authorization: REACT_APP_API_KEY,
+            Authorization: process.env.REACT_APP_API_KEY,
         }
     });
-    const jsonObj = await response.json();
-    await console.log(jsonObj.photos);
+    return response.json();
 }
 
 export default getPhotos;
