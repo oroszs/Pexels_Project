b9b415e7aa3d854325319b7baa1dbc1cd1b28af3
Sean Orosz <seanorosz@yahoo.com>
2022-02-16 11:42:24 -0500

    V1.2

--- src/index.js
+++ src/index.js
@@ -1,43 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import getPhotos from './service.js';
+import Images from './images.js';
 
 class App extends React.Component {
     render() {
         return (
             <Images />
-        );
-    }
-}
-
-class Images extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            imgs: {},
-        }
-    }
-
-    callApi() {
-        getPhotos('Pug').then(data => {
-            let photos = data.photos;
-            this.setState({
-                imgs: photos,
-            });
-        });
-    }
-    
-    componentDidMount(){
-        this.callApi();
-    }
-
-    render() {
-        let imgs = this.state.imgs;
-        console.log(imgs);
-        return(
-            <div>
-                {imgs[0] != undefined ? <img src={imgs[0].src.medium} alt='Blah'></img> : null}
-            </div>
         );
     }
 }
