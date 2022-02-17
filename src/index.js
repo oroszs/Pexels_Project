2fcdb6db2d393476098c226cac9abbf14ea97d29
Sean Orosz <seanorosz@yahoo.com>
2022-02-17 14:22:27 -0500

    V1.4

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Images from './images.js';
+import Search from './search.js';
+import getPhotos from './service.js';
 
 class App extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            images: undefined,
+        }
+        this.callApi = this.callApi.bind(this);
+    }
+
+    componentDidMount(){
+        this.callApi();
+    }
+
+    callApi(query) {
+        getPhotos(query).then(data => {
+            let photos = data.photos;
+            this.setState({
+                images: photos,
+            });
+        });
+    }
+
     render() {
+        let images = this.state.images;
         return (
             <div id='wrapper'>
-                <Images />
+                <Images images={images? images: undefined}/>
+                <Search callApi={this.callApi}/>
             </div>
         );
     }
--- src/service.js
+++ src/service.js
@@ -1,5 +1,8 @@
 const getPhotos = async (query) => {
-    const response = await fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=50`, {
+    let endpoint = (query === undefined) ? 
+    `https://api.pexels.com/v1/curated?per_page=50`:
+    `https://api.pexels.com/v1/search?query=${query}&per_page=50`;
+    const response = await fetch(endpoint, {
         headers: {
             Authorization: process.env.REACT_APP_API_KEY,
         }
