7564e5bc0eeed273ea793cef0f2b813531b1b2c6
Sean Orosz <seanorosz@yahoo.com>
2022-03-10 17:20:13 -0500

    modified variables in pages component to use consts instead of lets

--- src/components/pages.js
+++ src/components/pages.js
@@ -3,9 +3,9 @@ import React from 'react';
 class Pages extends React.Component {
 
     paginate() {
-        let numOfPages = this.props.numOfImages / 10;
-        let pageNums = [];
-        let currentPage = this.props.currentPage;
+        const numOfPages = this.props.numOfImages / 10;
+        const pageNums = [];
+        const currentPage = this.props.currentPage;
         for(let page = 1; page < numOfPages + 1; page++) {
             pageNums.push(
                 <div className={page !== currentPage ? 'activePageNumber pageNumber' : 'inactivePageNumber pageNumber'} key={page} onClick={() => this.props.pageClick(page)}>{page}</div>
@@ -15,7 +15,7 @@ class Pages extends React.Component {
     }
 
     render() {
-        let numOfImages = this.props.numOfImages;
+        const numOfImages = this.props.numOfImages;
         return (
             <div>
                 {numOfImages > 0 ? <div id='paginateDiv' className='componentDiv'>{this.paginate()}</div> : null}
